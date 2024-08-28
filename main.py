68b25757a1bc081506eeb408ec1cac2cf338e47c
eikeh <144679101+EikeHanel@users.noreply.github.com>
2024-08-28 08:11:10 +0200

    Gramma update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def str_to_morse_code(word_string):
             word_in_morse += "#"
         else:
             word_in_morse += morse_code[letter] + " "
-    print(f"Your word '{word_string}', converted into morse code is: {word_in_morse}")
+    print(f"Your word '{word_string}' has been converted into Morse code as: {word_in_morse}")
 
 
 print("Welcome to the Morse Code Converter!\nIf your input is a charter that is not a supported, "
@@ -31,8 +31,3 @@ while run:
     elif again != "n" and again != "y":
         print("Input was not clear program is being closed!")
         run = False
-
-
-
-
-
